9ff32e462048e27f01dec9fa2e11f395eab0dbbd
Wiktor <wiktor-kurka@wp.pl>
2021-03-05 00:16:25 +0100

    Initial commit

---
+++ Wyklad1/Program.cs
@@ -0,0 +1,29 @@
+﻿//deklaracja przestrzeni nazw
+using System;
+
+namespace Wyklad1
+{
+    //Nazwa Klasy
+    class Program
+    {
+
+        //Definicja funkcji Main - funkcji, w której wykonywane są polecenia po rozpoczęciu programu
+        static void Main(string[] args)
+        {
+            //Polecenie WriteLine klasy konsoli, które wyświetla tekst z argumentu typu ciągu
+            Console.WriteLine("Witaj w świecie!");
+
+            //Typy Danych:
+            bool a; //Typ Logiczny BOOLEAN                        (0 lub 1 / prawda lub fałsz)
+            byte b; //8-bitowa liczba całkowita                   (zakres 0 do 255)
+            char c; //16-bitowy znak tablicy Unicode     
+            decimal d; //128 bitowa liczba dziesiętna             (zakres (-7.9 x 10^28 do 7.9 x 10^28) / 10^(0 do 28))
+            double e; //64 bitowa zmienna double precision        (zakres (+/-)5.0 x 10^-324 do (+/-)1.7 x 10^308)
+            float f; //32 bitowa notacja naukowa                  (zakres -3.4 x 10^38 do + 3.4 x 10^38)
+            short g; //16 bitowa liczba całkowita                 (zakres -32768 do 32767)
+            int i; //32 bitowa liczba całkowita                   (zakres -2,147,483,648 do 2,147,483,647)
+            long j;//64 bitowa liczba całkowita                   (zakres -9,223,372,036,854,775,808 do 9,223,372,036,854,775,807)
+
+        }
+    }
+}
